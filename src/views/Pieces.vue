3db56986bc5cb0d1432c46a38e21feb31b61fff1
UzairDamonse <uzairdamonse03@gmail.com>
2022-07-11 15:02:31 +0200

    added user array

---
+++ src/views/Pieces.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>All of our pieces</h1>
+  <div v-for="piece in pieces" :key="piece.id"></div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      pieces: [],
+    };
+  },
+  mounted() {
+    fetch("https://http://localhost:3000/pieces")
+      .then((response) => response.json())
+      .then((json) => console.log(json));
+  },
+};
+</script>
+<style></style>
