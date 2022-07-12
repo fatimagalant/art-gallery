430b990c15a72b08c70555e2c6d8b8d46d565c29
UzairDamonse <uzairdamonse03@gmail.com>
2022-07-12 15:24:25 +0200

    finished store

--- src/store/index.js
+++ src/store/index.js
@@ -3,30 +3,86 @@ import { createStore } from "vuex";
 export default createStore({
   state: {
     pieces: null,
+    piece: null,
     users: null,
   },
   getters: {},
   mutations: {
-    setPieces(state, pieces) {
+    setPieces: (state, pieces) => {
       state.pieces = pieces;
     },
-    setUsers(state, users) {
-      state.users = users;
+    setPiece: (state, piece) => {
+      state.piece = piece;
+    },
+    setUser: (state, user) => {
+      state.user = user;
     },
   },
   actions: {
+    login: async (context, payload) => {
+      const { email, password } = payload;
+
+      const response = await fetch(
+        `http://localhost:3000/users?email=${email}&password=${password}`
+      );
+      const userData = await response.json();
+      context.commit("setUser", userData[0]);
+    },
+    register: async (context, payload) => {
+      const { fullName, email, password } = payload;
+
+      fetch("http://localhost:3000/users", {
+        method: "POST",
+        body: JSON.stringify({
+          fullName: fullName,
+          email: email,
+          password: password,
+        }),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      })
+        .then((response) => response.json())
+        .then((json) => console.log("setUser", json));
+    },
     getAllArt: async (context) => {
       fetch("http://localhost:3000/pieces")
         .then((res) => res.json())
         .then((pieces) => context.commit("setPieces", pieces));
     },
-    login: async(context, payload)=>{
-      const (email, password)=payload,
-
-      const response= await fetch{'http://localhost:3000/users'}
-
-      }
+    getSinglePiece: async (context) => {
+      fetch("http://localhost:3000/pieces" + id)
+        .then((res) => res.json())
+        .then((pieces) => context.commit("setPieces", pieces));
+    },
+    createPieces: async (context, piece) => {
+      fetch("http://localhost:3000/pieces", {
+        method: "POST",
+        body: JSON.stringify(piece),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      })
+        .then((response) => response.json())
+        .then(() => context.dispatch("getAllArt"));
+    },
+    updatePieces: async (context, piece) => {
+      fetch("http://localhost:3000/pieces" + piece.id, {
+        method: "PUT",
+        body: JSON.stringify(piece),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      })
+        .then((response) => response.json())
+        .then(() => context.dispatch("getAllArt"));
+    },
+    deletePiece: async (context, id) => {
+      fetch("http://localhost:3000/pieces" + id, {
+        method: "DELETE",
+      }).then(() => context.dispatch("getAllArt"));
     },
   },
+
   modules: {},
 });
--- src/views/Pieces.vue
+++ src/views/Pieces.vue
@@ -1,8 +1,13 @@
 <template>
-  <h1>All of our pieces</h1>
-  <div v-for="piece in pieces" :key="piece.id">
-    <h2>{{ piece.artName }}</h2>
-    <img v-bind:src="piece.imgURL" alt="" />
+  <div v-if="pieces">
+    <h1>All of our pieces</h1>
+    <div v-for="piece in pieces" :key="piece.id">
+      <h2>{{ piece.artName }}</h2>
+      <img v-bind:src="piece.imgURL" alt="" />
+    </div>
+  </div>
+  <div v-else>
+    <h1>Loading...</h1>
   </div>
 </template>
 <script>
